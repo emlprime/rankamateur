1773abae062b2cf16c1f1156fb3a89b6dffc10fd
Peter <peter@pop-os.localdomain>
2024-12-01 23:16:49 -0800

    add records to db

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,16 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
+import { candidateTable } from "./schema";
+import { eq } from "drizzle-orm";
 
 const db = drizzle(process.env.DATABASE_URL!);
+
+async function main() {
+  const candidate: typeof candidateTable.$inferInsert = {
+    name: "Elaina",
+  };
+
+  await db.insert(candidateTable).values(candidate);
+  console.log("im here");
+}
+main();
